0a81bdde0a724b3ef293037c5a427581a9da26e1
Julius Cebreros <me@juliuscebreros.com>
2015-07-04 06:29:37 +1200

    Initial Commit

---
+++ MechKeyboard/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MechKeyboard
+//
+//  Created by Julius Cebreros on 7/4/15.
+//  Copyright (c) 2015 Julius Cebreros. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
